b4ca4b168b590a17447672f33b3d72ab60224cfb
slandath <jesus_slanda@hotmail.com>
2022-12-31 19:03:33 -0500

    created login page
    forgot to add
    
    removed form pages
    
    is this it

--- src/Form.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import classes from "./Form.module.css";
-
-const Form = (props: any) => {
-  return <div className={classes.formContainer}>{props.children}</div>;
-};
-
-export default Form;
