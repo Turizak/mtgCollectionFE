68032257876438d7a4e4fc40008fcbc300c6656f
slandath <jesus_slanda@hotmail.com>
2023-01-21 23:49:48 -0500

    trying to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,12 @@
-import { resolve } from "path";
 import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react"
 
-const root = resolve(__dirname, "src");
-const outDir = resolve(__dirname, "dist");
 
 export default ({ mode }) => {
   // Load app-level env vars to node-level env vars.
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
   return defineConfig({
-    server: {
-      host: true,
-      port: 1337,
-      strictPort: true
-    },
-    root,
+    plugins: [react()]
   });
 };
