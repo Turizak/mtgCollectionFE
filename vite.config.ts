4d5353937c02448a4595419dbc8c523af6fe2d00
Tom Slanda <tomslanda@Toms-MacBook-Air.local>
2023-01-24 02:18:22 -0500

    fixed vite.config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react"
 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default ({ mode }) => {
-  // Load app-level env vars to node-level env vars.
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-
-  return defineConfig({
-    plugins: [react()]
+export default defineConfig({
+plugins: [react()]
   });
-};
