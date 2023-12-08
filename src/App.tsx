30be452521c7013073c680a9db9b3edfe26538e5
Tom Slanda <tomslanda@Toms-MacBook-Air.local>
2023-12-07 20:11:38 -0500

    removed notfound route

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import Login from "./Login";
-import MTGSearch from "./MTGSearch";
-import NotFound from "./NotFound";
-import Account from "./Account";
-import Collection from "./Collection";
+import Login from "./components/Login";
+import MTGSearch from "./components/MTGSearch";
+import Account from "./components/Account";
+import Collection from "./components/Collection";
 import PrivateRoutes from "./PrivateRoutes";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
@@ -12,7 +11,6 @@ function App() {
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/account" element={<Account />} />
-      <Route path="*" element={<NotFound />} />
       <Route element={<PrivateRoutes />}>
         <Route path="/collection" element={<Collection />} />
         <Route path="/search" element={<MTGSearch />} />
--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,4 +1,4 @@
-const NotFound = () => {
+function NotFound() {
   return (
     <div>
       <h1>Page Not Found!</h1>
