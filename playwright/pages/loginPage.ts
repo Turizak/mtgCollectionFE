a2a630b4c7e9f1807629b3809b140e05f650a4c5
Rob K <kaz.brah@gmail.com>
2023-12-10 23:27:07 -0500

    boiler plate playwright configuration and login page stuff

---
+++ playwright/pages/loginPage.ts
@@ -0,0 +1,40 @@
+import { type Locator, type Page } from "@playwright/test";
+
+export class LoginPage {
+  readonly page: Page;
+  readonly btnLogin: Locator;
+  readonly btnCreateAccount: Locator;
+  readonly inputUsername: Locator;
+  readonly inputPassword: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.btnLogin = page.getByText("Login");
+    this.btnCreateAccount = page.getByText("Create Account");
+    this.inputUsername = page.locator("#username");
+    this.inputPassword = page.locator("#password");
+  }
+
+  /**
+   * Navigate to the form page
+   * @async
+   * @method
+   */
+  async goto() {
+    await this.page.goto("/");
+  }
+
+  /**
+   * Fill out the login form page via UI
+   * @async
+   * @method
+   * @param {String} options.username string
+   * @param {String} options.password string
+   */
+  async loginUI(options: { username: string; password: string }) {
+    // input fields
+    await this.inputUsername.fill(options.username);
+    await this.inputPassword.fill(options.password);
+    await this.btnLogin.click();
+  }
+}
---
+++ playwright/tests/login.spec.ts
@@ -0,0 +1,61 @@
+import { test, expect } from "@playwright/test";
+import { LoginPage } from "../pages/loginPage";
+
+type FormOptions = {
+  username: string;
+  password: string;
+};
+
+test.describe("Login Page Tests", () => {
+  test("Successful login with valid username and password", async ({
+    page,
+  }) => {
+    const loginPage = new LoginPage(page);
+
+    const formData: FormOptions = {
+      username: process.env.PLAYWRIGHT_USER as string,
+      password: process.env.PLAYWRIGHT_PASS as string,
+    };
+
+    await test.step("Navigate to the page", async () => {
+      await loginPage.goto();
+    });
+
+    await test.step("Fill out form and submit", async () => {
+      await loginPage.loginUI(formData);
+    });
+
+    await test.step("Assertion", async () => {
+      await page.waitForURL("/collection");
+      expect(page.url()).toContain("/collection");
+    });
+  });
+
+  test("Unsuccesful login with invalid username and password", async ({
+    page,
+  }) => {
+    const loginPage = new LoginPage(page);
+
+    const formData: FormOptions = {
+      username: "fakeuser",
+      password: "fakepassword",
+    };
+
+    await test.step("Navigate to the page", async () => {
+      await loginPage.goto();
+    });
+
+    await test.step("Fill out form and submit", async () => {
+      await loginPage.loginUI(formData);
+    });
+
+    await test.step("Assertion", async () => {
+      await Promise.all([
+        page.waitForResponse(
+          (resp) =>
+            resp.url().includes("/api/v1/login") && resp.status() === 400
+        ),
+      ]);
+    });
+  });
+});
