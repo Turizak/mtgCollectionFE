3179824bebe95db3bec3e2ad2073645ba42b3a7b
Rob K <kaz.brah@gmail.com>
2023-12-11 15:08:01 -0500

    add create account test and data cleanup

---
+++ playwright/pages/collectionPage.ts
@@ -0,0 +1,18 @@
+import { type Locator, type Page, type Request } from "@playwright/test";
+
+export class CollectionPage {
+  readonly page: Page;
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  /**
+   * Navigate to the page
+   * @async
+   * @method
+   */
+  async goto() {
+    await this.page.goto("/collection");
+  }
+}
